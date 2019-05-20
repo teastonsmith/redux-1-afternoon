703a054cf2c6ac38400d396f46dfaaacce3496c0
teastonsmith2 <t.easton.smith2@gmail.com>
2019-05-20 14:14:12 -0600

    step 3 complete

--- src/store.js
+++ src/store.js
@@ -1,1 +1,29 @@
+import { createStore } from 'redux';
 
+const initialState = {
+	recipe: [],
+	category: [],
+};
+
+export const UPDATE_RECIPE_NAME = 'UPDATE_RECIPE_NAME';
+export const UPDATE_RECIPE_CATEGORY = 'UPDATE_RECIPE_CATEGORY';
+
+function reducer(state = initialState, action) {
+	const { type, payload } = action;
+	switch (type) {
+		case UPDATE_RECIPE_NAME:
+			return {
+				...state,
+				recipe: action.recipe,
+			};
+		case UPDATE_RECIPE_CATEGORY:
+			return {
+				...state,
+				category: action.category,
+			};
+		default:
+			return state;
+	}
+}
+
+export default createStore(reducer);
